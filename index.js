93416dea3319ecd09c8052263e435399755aa82d
Gourav gosain <40332708+gosaingourav17@users.noreply.github.com>
2022-07-20 12:02:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+app.use('/', require('./api/routes/crud'))
 var port = process.env.PORT||3000
 const srever = http.createServer(app);
 srever.listen(port);
